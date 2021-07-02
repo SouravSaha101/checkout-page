ff0f3ca398f34e9e4e4ce7a7391c255742c48624
Sourav <souravsaha101@gmail.com>
2021-07-02 18:22:59 +0530

    offers section added

---
+++ src/components/OffersBox/Offersbox.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Offersbox.css";
+
+function Offersbox() {
+  return (
+    <div>
+      <div className="coupons-base-header"> Offers & Coupons</div>
+      <div>
+        <h6> Offers in bag</h6>
+      </div>
+    </div>
+  );
+}
+
+export default Offersbox;
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Sidebar.css";
+import Offersbox from "../OffersBox/Offersbox";
 
 const Sidebar = () => {
-  return <div className="sidebar">SideBar</div>;
+  return (
+    <div className="sidebar">
+      <Offersbox />
+    </div>
+  );
 };
 
 export default Sidebar;
