68509f3f6e0982561ef22c75cb6ae4f19fa5620b
Sourav <souravsaha101@gmail.com>
2021-07-03 23:52:56 +0530

    data seperated

---
+++ src/Cartdata.js
@@ -0,0 +1,30 @@
+const itemList = [
+  {
+    id: 1,
+    name: "Brown Brim",
+    brand: "Nike",
+    imageUrl: "https://i.ibb.co/ZYW3VTp/brown-brim.png",
+    price: 2500,
+    soldBy: "Seller Best",
+    discount: 50,
+    deliveryDate: "5 Jul 2021",
+    size: "M",
+    qty: 1,
+    orgPrice: 5000,
+  },
+  {
+    id: 2,
+    name: "Blue Jean Jacket",
+    brand: "Pepe",
+    imageUrl: "https://i.ibb.co/mJS6vz0/blue-jean-jacket.png",
+    price: 6300,
+    soldBy: "New India",
+    discount: 30,
+    deliveryDate: "9 Jul 2021",
+    size: "L",
+    qty: 2,
+    orgPrice: 9000,
+  },
+];
+
+export default itemList;
